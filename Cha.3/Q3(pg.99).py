edc26501f17feb8ae5990ba93fe118bd81c7bb86
eunji0123 <73058457+eunji0123@users.noreply.github.com>
2021-07-29 18:16:08 +0900

    Add another solution

--- Cha.3/Q3(pg.99).py
+++ Cha.3/Q3(pg.99).py
@@ -18,3 +18,15 @@ print(ans)
 
 n, k = map(int,input().split())
 ans = 0
+
+while True:
+    # n을 k의 배수 중 최대로 일단 만듦
+    target = (n // k) * k
+    if target != 0:
+        ans += (n - target) + 1 # target이 될 때까지 빼고 k의 배수가 됐으니까 1번 나눠주는 것까지 연산
+        n = target // k
+    else: # target이 0이라는 건, 결국 n을 k로 나눌 수 없다는 얘기
+        break
+
+ans += n - 1 # 1이 될때까지 계속 빼주는 연산
+print(ans)

d009c84fd3ee1392cdbfe06b7285392741bbfcaf
eunji0123 <73058457+eunji0123@users.noreply.github.com>
2021-08-07 16:30:25 +0900

    Place function first

--- Cha.5/DFS.py
+++ Cha.5/DFS.py
@@ -1,3 +1,12 @@
+def dfs(graph, v, visited):
+    # 방문 처리
+    visited[v] = True
+    print(v, end = ' ')
+    # 인접 노드 처리
+    for i in graph[v]:
+        if not visited[i]: # 방문하지 않은 인접 노드일 경우
+            dfs(graph, i, visited)
+
 graph = [[],
          [2, 3, 8],
          [1, 7],
@@ -7,17 +16,7 @@ graph = [[],
          [7],
          [2, 6, 8],
          [1, 7]]
-visited = [False] * 9
-
-
-def dfs(graph, v, visited):
-    # 방문 처리
-    visited[v] = True
-    print(v, end = ' ')
-    # 인접 노드 처리
-    for i in graph[v]:
-        if not visited[i]: # 방문하지 않은 인접 노드일 경우
-            dfs(graph, i, visited)
+visited = [False] * 9                  
 
 dfs(graph, 1, visited)
 

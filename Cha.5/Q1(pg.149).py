30cb969896b622d2a37e9b020c11d11199a39298
eunji0123 <73058457+eunji0123@users.noreply.github.com>
2021-08-13 18:01:56 +0900

    Added my second solution using DFS

--- Cha.5/Q1(pg.149).py
+++ Cha.5/Q1(pg.149).py
@@ -38,4 +38,37 @@ for j in range(n):
 
 print(count)
 
+####################################################
 
+# My solution 2 - Using DFS
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+count = 0
+
+def dfs(v):
+    graph[v[0]][v[1]] = 1
+    for i in range(4):
+        x = v[0] + dx[i]
+        y = v[1] + dy[i]
+        if x < 0 or x >= n or y < 0 or y >= m:
+                continue
+        if graph[x][y] == 0:
+            dfs((x, y))
+
+n, m = map(int, input().split())
+graph = []
+
+for _ in range(n):
+    row = list(map(int, list(input())))
+    graph.append(row)
+
+#visited = [[False] * m for _ in range(n)]
+
+for j in range(n):
+    for k in range(m):
+        if graph[j][k] == 0:
+            dfs((j, k))
+            count += 1
+
+print(count)

c12f2a810046f5f1a37347e225f673565c723fc9
eunji0123 <73058457+eunji0123@users.noreply.github.com>
2022-01-11 16:31:22 +0900

    Update and rename temp.py to Q1(pg.182).py

--- Cha.6/Q1(pg.182).py
+++ Cha.6/Q1(pg.182).py
@@ -1,3 +1,5 @@
+# My solution
+
 n, k = map(int, input().split())
 
 a = list(map(int, input().split()))
@@ -20,15 +22,19 @@ for i in range(k):
 print(sum(a))
 
 # remove()는 O(N)의 시간이 소요되므로 남발하면 시간 초과될 수 있음. 지양하기. (insert도 마찬가지)
-    
-'''
 
+######################################################
+
+# 답지
+
+'''
+# 한 번 정렬을 먼저 해주면 전체 연산 횟수는 down
 a.sort()
 b.sort(reverse = True)
 
 for i in range(k):
     if a[i] < b[i]:
-        a[i], b[i] = b[i], a[i]
+        a[i], b[i] = b[i], a[i] # 서로 다른 리스트의 원소도 swap 가능
     else:
         break
 
